17434f8b8cab49ed34f0902e2772a2e243a35848
Pablo Lopes <pabloelopes@hotmail.com>
2021-03-07 18:20:59 -0300

    Final Commit

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -59,7 +59,7 @@ const About: React.FC = () => {
               <ApiCard />
             </div>
             <div className="downloadArtigo">
-              <a href="https://drive.google.com/file/d/1Uk4idqAhDqJdclAiZHf9iyjdd3Mrymfm/view" target="_blank">
+              <a rel="noopener noreferrer" href="https://drive.google.com/file/d/1Uk4idqAhDqJdclAiZHf9iyjdd3Mrymfm/view" target="_blank">
                 <CardContainer className="btnTech"> 
                   <AiFillFileText size={150} className="techIcon"/>
                   <p>Baixe o artigo</p>
